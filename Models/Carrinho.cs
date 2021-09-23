570454f565336d7890e5510906c72570e7b351bf
CleitonFurst <43749359+CleitonFurst@users.noreply.github.com>
2021-09-22 21:58:05 -0300

    first commit

---
+++ Models/Carrinho.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Pneus.Models
+{
+    public class Carrinho
+    {
+        public int Id { get; set; }
+        public int Quant { get; set; }
+
+        public int? produtosId { get; set; }
+
+        public List<Produtos> produtos { get; set; }
+
+    }
+}
---
+++ Models/Produtos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Pneus.Models
+{
+    public class Produtos
+    {
+        public int Id { get; set; }
+        public string NomeProd { get; set; }
+        public string  Descricao { get; set; }
+        public double Preco { get; set; }       
+        public string Marca { get; set; }
+        public string Img { get; set; }
+        public int carrinhoId { get; set; }
+        public Carrinho carrinho { get; set; }
+
+
+    }
+}
