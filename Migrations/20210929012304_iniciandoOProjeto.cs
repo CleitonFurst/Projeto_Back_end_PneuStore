a0979cae04abb73838c85d3848a9d123f1c58eba
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2021-09-28 23:00:17 -0300

    Funcional

---
+++ Migrations/20210929012304_iniciandoOProjeto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_Pneus.Migrations
+{
+    public partial class iniciandoOProjeto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
